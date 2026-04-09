cec47dfa9fa29ecbe6c758a436f96bb2ef0a01ad
GakuruAlex <gakurualexgicimo@gmail.com>
2026-04-09 21:06:17 +0300

    Login form using dynamic form

--- lib/widgets/form/login_form.dart
+++ lib/widgets/form/login_form.dart
@@ -7,24 +7,17 @@ import 'package:kodisha_flutter/pages/landlord/landlord_homepage.dart';
 import 'package:kodisha_flutter/screens/login.dart';
 import 'package:kodisha_flutter/widgets/form/dynamic_form/dynamic_form.dart';
 
-class LoginForm extends ConsumerStatefulWidget {
-  const LoginForm({super.key});
+class LoginForm extends ConsumerWidget {
+  LoginForm({super.key});
 
-  @override
-  ConsumerState<LoginForm> createState() => _LoginFormState();
-}
-
-class _LoginFormState extends ConsumerState<LoginForm> {
-  final _formKey = GlobalKey<FormState>();
   final controllers = {
     "emailaddress": TextEditingController(),
     "password": TextEditingController(),
   };
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final loginSuccess = ref.watch(loginNotifier);
-    final loginProvider = ref.read(loginNotifier.notifier);
 
     ref.listen<AuthRoleState>(authRoleProvider, (prev, next) {
       switch (next) {
@@ -50,80 +43,57 @@ class _LoginFormState extends ConsumerState<LoginForm> {
       }
     });
 
-    return Form(
-      key: _formKey,
-      child: SizedBox(
-        height: MediaQuery.sizeOf(context).height * .7,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            SizedBox(
-              height: MediaQuery.sizeOf(context).height * .31,
-              width: MediaQuery.sizeOf(context).width * .9,
+    return SizedBox(
+      height: MediaQuery.sizeOf(context).height * .7,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SizedBox(
+            height: MediaQuery.sizeOf(context).height * .4,
+            width: MediaQuery.sizeOf(context).width * .9,
 
-              child: DynamicForm(
-                controllers: controllers,
-                formType: "Login",
-                fields: [
-                  DynamicFormField(
-                    fieldLabel: "Email Address",
-                    textInputType: TextInputType.emailAddress,
-                    fieldIcon: Icons.email,
-                  ),
-                  DynamicFormField(
-                    fieldLabel: "Password",
-                    textInputType: TextInputType.text,
-                    fieldIcon: Icons.password,
-                  ),
-                ],
-              ),
+            child: DynamicForm(
+              constraints: {
+                "height ": MediaQuery.sizeOf(context).height * .4,
+                "width": MediaQuery.sizeOf(context).width * .9,
+                "pad": 20.0,
+                "tileWidth": MediaQuery.sizeOf(context).width * .4,
+              },
+              controllers: controllers,
+              formType: "Login",
+              buttonIcon: Icons.login,
+              fields: [
+                DynamicFormField(
+                  fieldLabel: "Email Address",
+                  textInputType: TextInputType.emailAddress,
+                  fieldIcon: Icons.email,
+                ),
+                DynamicFormField(
+                  fieldLabel: "Password",
+                  textInputType: TextInputType.text,
+                  fieldIcon: Icons.password,
+                ),
+              ],
             ),
-
-            SizedBox(
-              width: MediaQuery.sizeOf(context).width * .5,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 12.0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      loginProvider.loginUser(
-                        controllers["emailaddress"]!.text,
-                        controllers["password"]!.text,
-                      );
-                    }
-                  },
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.login,
-                      color: Theme.of(context).colorScheme.inversePrimary,
-                    ),
-                    title: Text(
-                      "Login",
-                      style: Theme.of(context).textTheme.bodySmall,
-                    ),
+          ),
+          SizedBox(
+            child: loginSuccess.when(
+              data: (data) => const SizedBox.shrink(),
+              error: (error, statck) => Card(
+                color: Theme.of(context).colorScheme.errorContainer,
+                child: Text(
+                  "$error",
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.onErrorContainer,
                   ),
                 ),
               ),
+              loading: () => CircularProgressIndicator(),
             ),
-            SizedBox(
-              child: loginSuccess.when(
-                data: (data) => const SizedBox.shrink(),
-                error: (error, statck) => Card(
-                  color: Theme.of(context).colorScheme.errorContainer,
-                  child: Text(
-                    "$error",
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.onErrorContainer,
-                    ),
-                  ),
-                ),
-                loading: () => CircularProgressIndicator(),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
