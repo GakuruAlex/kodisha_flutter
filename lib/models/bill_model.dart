e87d9dffa61ad083311581c89bd3d8f8065d1275
GakuruAlex <gakurualexgicimo@gmail.com>
2026-04-24 22:03:58 +0300

    Create a bill model

---
+++ lib/models/bill_model.dart
@@ -0,0 +1,14 @@
+import 'package:kodisha_flutter/models/form_model.dart';
+
+class BillModel extends FormModel {
+  BillModel({required this.name, required this.accountNumber, this.id});
+
+  final String name;
+  final int? id;
+  final String accountNumber;
+
+  @override
+  Map<String, dynamic> toFormValues() {
+    return {"name": name, "account number": accountNumber};
+  }
+}
